c127972790bd5de5ebeafa16c730115e6dc092b3
Stelios Papadopoulos <stylianosplus@gmail.com>
2026-03-04 08:50:08 +0200

    changed axios url

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://project-management-api.steliospap.com",
+  baseURL: "http://vgccgg4gwgow4oow08wwksck.188.34.191.111.sslip.io/",
 });
 
 export default api;
